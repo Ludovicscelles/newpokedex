4eaaded9d1a0a0b15b4f78c3d4949695d3ec4206
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-05-09 16:14:29 +0200

    creation of components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,29 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
     <section>
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? (
+          <img
+            className="imageCard"
+            src={pokemon.imgSrc}
+            alt={pokemon.name}
+          ></img>
+        ) : (
+          "???"
+        )}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </section>
   );
